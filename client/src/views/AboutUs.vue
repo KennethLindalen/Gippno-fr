47c600936c62ee866b2b34927d3ff465877f05ee
kennethlindalen <kennethlindalen@gmail.com>
2019-11-13 16:19:49 +0100

    Created About site

---
+++ client/src/views/AboutUs.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="jumbotron pt-3 pb-2">
+    <h1>About us</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutUs',
+};
+</script>
+
+<style scoped>
+
+</style>
