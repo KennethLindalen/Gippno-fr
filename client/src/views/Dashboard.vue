575f65b47e04e65774712c158835e76a18b28140
kennethlindalen <kennethlindalen@gmail.com>
2019-11-13 16:20:19 +0100

    Test data for Dashboard

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,6 +1,72 @@
 <template>
 <div class="jumbotron  pt-3 pb-4">
   <h1>Dashboard</h1>
+  <div class="card mt-3 mb-1">
+    <div class="card-body">
+      <h3>Messages</h3>
+      <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Navn på avsender 1</h5>
+            <small>15min ago</small>
+          </div>
+          <p class="mb-1">Donec id elit non mi porta gravida at eget metus.
+            Maecenas sed diam eget risus varius blandit.</p>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Navn på avsender 2</h5>
+            <small>15min ago</small>
+          </div>
+          <p class="mb-1">Donec id elit non mi porta gravida at eget metus.
+            Maecenas sed diam eget risus varius blandit.</p>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Navn på avsender 3</h5>
+            <small>15min ago</small>
+          </div>
+          <p class="mb-1">Donec id elit non mi porta gravida at eget metus.
+            Maecenas sed diam eget risus varius blandit.</p>
+        </a>
+      </div>
+      <br>
+      <button type="button" class="btn btn-primary" style="width: 100%">View messages</button>
+    </div>
+  </div>
+  <div class="card mt-3 mb-1">
+    <div class="card-body">
+      <h3>Your projects</h3>
+      <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Prosjekt navn 1</h5>
+            <small>Created 3 days ago</small>
+          </div>
+          <p class="mb-1">Donec id elit non mi porta gravida at eget metus.
+            Maecenas sed diam eget risus varius blandit.</p>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Prosjekt navn 2</h5>
+            <small>Created 3 days ago</small>
+          </div>
+          <p class="mb-1">Donec id elit non mi porta gravida at eget metus.
+            Maecenas sed diam eget risus varius blandit.</p>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Prosjekt navn 3</h5>
+            <small>Created 3 days ago</small>
+          </div>
+          <p class="mb-1">Donec id elit non mi porta gravida at eget metus.
+            Maecenas sed diam eget risus varius blandit.</p>
+        </a>
+      </div>
+      <br>
+      <button type="button" class="btn btn-primary" style="width: 100%">New project</button>
+    </div>
+  </div>
 </div>
 </template>
 
