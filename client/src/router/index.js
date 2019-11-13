e0c3377503774a69b994e97ec4a0ccd11c1bb6be
kennethlindalen <kennethlindalen@gmail.com>
2019-11-13 16:19:34 +0100

    Proper redirects

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -5,24 +5,47 @@ import Signup from '../views/Signup.vue';
 import Login from '../views/Login.vue';
 import News from '../views/News.vue';
 import Dashboard from '../views/Dashboard.vue';
-
+import AboutUs from '../views/AboutUs.vue';
+/* eslint-disable */
 Vue.use(VueRouter);
+
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: Home,
+    beforeEnter (to, from, next){
+      if(localStorage.token){
+        next('/dashboard');
+      }else {
+        next();
+      }
+    },
   },
   {
     path: '/signup',
     name: 'signup',
     component: Signup,
+    beforeEnter (to, from, next){
+      if(localStorage.token){
+        next('/dashboard');
+      }else{
+        next();
+      }
+    },
   },
   {
     path: '/login',
     name: 'login',
     component: Login,
+    beforeEnter (to, from, next){
+      if(localStorage.token){
+        next('/dashboard');
+      }else{
+        next();
+      }
+    },
   },
   {
     path: '/news',
@@ -30,13 +53,27 @@ const routes = [
     component: News,
   },
   {
+    path: '/about',
+    name: 'about',
+    component: AboutUs,
+  },
+  {
     path: '/dashboard',
     name: 'dashboard',
     component: Dashboard,
+    beforeEnter (to, from, next){
+      if(!localStorage.token){
+        next('/login');
+      }else{
+        next()
+      }
+    }
   },
 ];
 
 const router = new VueRouter({
+  mode: 'history',
+  hash: 'none',
   routes,
 });
 
